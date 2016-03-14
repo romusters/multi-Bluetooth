9f5b8f76d163af0b5b7225222262f5facebab416
romusters <romusters@gmail.com>
2016-03-14 17:48:27 +0100

    Protocol A is implemented for a single character.

--- arduino.py
+++ arduino.py
@@ -1,11 +1,64 @@
 import bluetooth
+import sys
+
+
 def main():
 	mod1_addr = "98:D3:31:FC:0E:DC"
 	port = 1
 	sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 	sock.connect((mod1_addr, port))
-	sock.send("hello!!")
+
+
+
+	sock.send("Ksa")
+	letter = "a"
+	print "Send message"
+
+
+	message = ""
+	KsKr = False
+	while(not KsKr):
+		try:
+			message = message + sock.recv(32)
+			print message
+			if ("Kr" + letter) in message:
+				KsKr = True
+		except Exception:
+			import traceback
+			traceback.print_exc()
+			KsKr = True
+
+	print "KsKr established."
+	message = ""
+	KsKrKsKr = False
+ 	while(not KsKrKsKr):
+		sock.send("KsKr" + letter)
+		try:
+			message = message + sock.recv(32)
+			print message
+			if ("KrKsKr" + letter) in message:
+				print "Victory"
+				KsKrKsKr = True
+		except Exception:
+			import traceback
+			traceback.print_exc()
+			KsKrKsKr = True
+
 	sock.close()
+	return 0
+
+	#
+	# recsock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
+	# port = 1
+	# recsock.bind(("",port))
+	# print recsock.listen(port)
+	#
+	# client_sock, address = recsock.accept()
+	# print "Accepted connection from ", address
+	#
+	# data = client_sock.recv(1024)
+	# print "received [%s]" % data
+	# recsock.close()
 
 def printDevices():
 	nearby_devices = bluetooth.discover_devices()
@@ -16,3 +69,4 @@ def printDevices():
 
 if __name__ == "__main__":
 	main()
+	#printDevices()
--- bluetoothStandalone/bluetoothStandalone.ino
+++ bluetoothStandalone/bluetoothStandalone.ino
@@ -3,8 +3,6 @@
 int voltPin = 8;
 int keyPin = 9;
 
-
-
 void setup() {
   Serial.begin(38400);
 
@@ -47,19 +45,28 @@ void rebootAT(bool nowait=false){
   }
 }
 
+String readData(){
+  String str = "";
+  if(Serial.available() > 0){  
+    delay(3);
+    while (Serial.available () > 0) {
+      str += char(Serial.read ());
+    }
+  }
+  str.trim();
+  return str;
+}
+
 
 String str = "";
-    
+char letter;   
+int i = 0;
+bool Kr = false;
+bool KrKsKr = false;
+bool Krnew = false;
 
 void loop() {
-    if(Serial.available() > 0){  
-      delay(3);
-      while (Serial.available () > 0) {
-        str += char(Serial.read ());
-      }
-      str.trim();
-      Serial.println(str);
-      
+     
       if(str == "a"){
         rebootAT();
       }
@@ -75,8 +82,63 @@ void loop() {
         delay(1000);
         rebootNormal();
       }
-      str = "";
-    }
+      
+      Serial.print("Length of the string is: ");
+      Serial.println(str.length());
+
+      while(!Kr){
+        str = readData();
+        
+        if(str.length() == 3){
+          letter = str.charAt(2);
+          //Serial.print("Received: ");
+          //Serial.println(letter);
+          Serial.write("Kr");
+          Serial.write(letter);
+          Serial.write("\r\n");
+          str = "";
+          Kr = true;
+          Krnew = false;
+          delay(100);
+        }
+        delay(100);
+      }
+      
+
+      while(!KrKsKr){
+        str = readData();
+        
+        if(str.length() == 5){
+          if(str.charAt(4) == letter){
+            str = "";
+            KrKsKr = true;
+            delay(100);
+          }
+        }
+        delay(100);
+      }
+      
+
+      while(!Krnew){
+        Serial.write("KrKsKr");
+        Serial.write(letter);
+        Serial.write("\r\n");
+        delay(10);
+        str = readData();
+        if(str.length() == 3) {
+          letter = str.charAt(2);
+          Krnew = true;
+          i += 1;
+          Serial.println("New letter received");
+          Kr = false;
+          KrKsKr = false;
+          delay(100);
+        }
+        delay(100);
+        Krnew = true; //REMOVE IN FUTURE
+      }
+      
+
     delay(1000);
-    Serial.println("Running");
+    //Serial.println("Running");
 }
